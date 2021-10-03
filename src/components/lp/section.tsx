06ccf7c8ea5bc0aa6a6a58ac9a875a72c2426893
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-10-03 10:39:37 +0900

    fix: changed the style of section

--- src/components/lp/section.tsx
+++ src/components/lp/section.tsx
@@ -12,21 +12,15 @@ const Section: FC<{ title: string; imageUrl: string; isTextLeft: boolean; patter
         src={props.patternUrl}
         alt=""
       />
-      <div className="m-auto relative md:flex md:items-center">
-        {props.isTextLeft ? <div className="md:flex-grow md:order-1"></div> : null}
+      <div className="m-auto max-w-5xl relative md:flex md:items-center">
         <h2
-          className={`text-4xl font-medium mb-16 md:w-1/2 md:max-w-lg md:order-2 ${
-            props.isTextLeft ? 'md:pr-6' : 'md:pl-6'
+          className={`text-4xl font-medium mb-16 md:w-1/2 ${
+            props.isTextLeft ? 'md:pr-6' : 'md:pl-6 md:order-2'
           }`}
         >
           {props.title}
         </h2>
-        <img
-          className={`shadow md:w-1/2  ${props.isTextLeft ? 'md:order-3' : 'md:order-1'}`}
-          src={props.imageUrl}
-          alt={props.title}
-        />
-        {props.isTextLeft ? null : <div className="md:flex-grow md:order-3"></div>}
+        <img className="shadow md:w-1/2" src={props.imageUrl} alt={props.title} />
       </div>
     </div>
   )
